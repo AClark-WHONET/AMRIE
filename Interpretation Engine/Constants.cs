52d61dffa71525e4a39a0693623db05215485de0
AClark <aclark@whonet.org>
2025-09-04 13:06:19 -0400

    Exclude Moraxella spp. from the EUCAST non-fermenting rods definition for expected resistance phenotypes.

--- Interpretation Engine/Constants.cs
+++ Interpretation Engine/Constants.cs
@@ -13,7 +13,7 @@ namespace AMR_Engine
 		// This should increase whenever an issue is resolved
 		// within a year to indicate that the table itself has changed.
 		// For example, if there is a document correction or error on our part.
-		public static readonly int BreakpointTableRevisionMinorChangeNumber = 3;
+		public static readonly int BreakpointTableRevisionMinorChangeNumber = 4;
 
 		// We use this to locate resources.
 		public static string SystemRootPath =
